b8c20a44ac085b8c03c404dc10a08510223999ad
Caeron Seabourne <caeking93@gmail.com>
2018-07-05 21:13:11 +0100

    Added the BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks.js'
-import BookListing from './BookListing.js'
+import BookShelf from './BookShelf.js'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -12,7 +12,16 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    bookDetails: [] //This array holds an array of objects, one for each book.
+  }
+
+  // This will fetch all books from the API and store them in the bookDetails array.
+  componentDidMount() {
+    BooksAPI.getAll().then((bookDetails) => {
+      this.setState({ bookDetails })
+      console.log(this.state.bookDetails)
+    })
   }
 
   render() {
@@ -28,33 +37,12 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <BookListing />
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Enders Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+              <BookShelf
+                //Need to pass in the books on the shelf
+              />
+
+              {/*Repeat the above BookShelf for the other 2 types.*/}
+              
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
@@ -160,7 +148,6 @@ class BooksApp extends React.Component {
                   </div>
                 </div>
               </div>
-            </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
--- src/BookListing.js
+++ src/BookListing.js
@@ -1,10 +1,13 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+//import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-function BookListing() {
-  return (
-    <li>
+class BookListing extends React.Component {
+
+  //Get the books and swap out book data below for dynamic data
+
+  render() {
+    return (
      <div className="book">
        <div className="book-top">
          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -21,8 +24,8 @@ function BookListing() {
        <div className="book-title">To Kill a Mockingbird</div>
        <div className="book-authors">Harper Lee</div>
      </div>
-    </li>
-  )
+    )
+  }
 }
 
 export default BookListing;
